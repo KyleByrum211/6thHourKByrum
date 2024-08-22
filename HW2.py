db42a284e7cf2bc705e741cfb199b8d9c22cbcd1
Kyle Byrum <kb126027@student.wpsok.org>
2024-08-22 11:42:18 -0700

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,19 @@
+#Kyle Byrum
+#6th Hour
+#Assignment: HW2
+
+#Print Hello World!
+print("Hello World!")
+
+#Make an input that asks for your name.
+Name = input("Please type your name: ")
+print(f"Hello {Name}!")
+
+#Make two inputs that ask for two integers.
+Integer1 = int(input("Please provide an integer: "))
+Integer2 = int(input("Please provide a second integer: "))
+
+#Add those two integers together and print the sum.
+print("The sum of your numbers is: ", Integer1 + Integer2)
+
+#Chat is this real?
