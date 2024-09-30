3ecb79e5e6d2fae036bfca26b2f1323f0d949700
Kyle Byrum <kb126027@student.wpsok.org>
2024-09-30 13:53:15 -0500

    Changed HW6 to be slower

--- HW6.py
+++ HW6.py
@@ -2,8 +2,8 @@
 #Class: 5th Hour
 #Assignment: HW6
 
-
-#Objectives
+#Imports
+import time
 
 #Print Hello World!
 print("Hello World!")
@@ -17,6 +17,17 @@ while True:
         break
 
     #Create a nested if statement to check if the number is divisible by 2 or 3
+    time.sleep(0.5)
+    print("Calculating.")
+    time.sleep(2)
+    print(".")
+    time.sleep(2)
+    print("..")
+    time.sleep(2)
+    print("...")
+    time.sleep(2)
+    print("Calculating finished.")
+    time.sleep(2)
     if num%2 == 0:
         if num%3 == 0:
             print(f"{num} divided by 2 is: {int(num / 2)}")
@@ -30,5 +41,6 @@ while True:
             print(f"{num} divided by 3 is: {int(num / 3)}")
         else:
             print(f"{num} is not divisible by 2 or 3.")
+    time.sleep(2)
     print("")
 
