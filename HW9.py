ec65d14a7da8946ab06fc9636582b5f1d3edec72
Kyle Byrum <kb126027@student.wpsok.org>
2024-10-24 13:32:51 -0500

    Edited HW9 so it is american

--- HW9.py
+++ HW9.py
@@ -4,7 +4,7 @@
 
 #Variables and Imports
 import random
-temperature = random.randint(1, 25)
+temperature = random.randint(1, 100)
 
 #1. Print Hello World!
 print("Hello World!")
@@ -12,9 +12,9 @@ print("Hello World!")
 #2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
 #https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
 
-if temperature > 20:
+if temperature > 75:
     print("It's hot")
-elif temperature > 10:
+elif temperature > 40:
     print("It's mild")
 else:
     print("It's cold")
